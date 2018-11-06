e279aaa01982901ff3120eca9f174b06a07a3f59
astana-kitap <44795644+astana-kitap@users.noreply.github.com>
2018-11-06 11:57:30 +0600

    Create OscillationZ.py

---
+++ OscillationZ.py
@@ -0,0 +1,15 @@
+GlowScript 2.7 VPython
+
+s=sphere(pos=vector(0,0,0), color=vector(0,0,1), radius=4)
+
+for i in range(400):
+    rate(30)
+    
+    position = 10*sin(2*pi*i/100.0)
+    
+    s.pos=vector(0, 0, position)
+    
+    
+    
+    
+    
