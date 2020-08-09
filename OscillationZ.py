07d785c89843f2d6426f0a4f05a4010e7a896db1
Lm004yky <69414954+Lm004yky@users.noreply.github.com>
2020-08-09 13:31:20 +0600

    Update OscillationZ.py

--- OscillationZ.py
+++ OscillationZ.py
@@ -1,6 +1,6 @@
 GlowScript 2.7 VPython
 
-s=sphere(pos=vector(0,0,0), color=vector(0,0,1), radius=4)
+s=sphere(pos=vector(position,position,0), color=vector(1,1,1), radius=4)
 
 for i in range(400):
     rate(30)
@@ -8,6 +8,8 @@ for i in range(400):
     position = 10*sin(2*pi*i/100.0)
     
     s.pos=vector(0, 0, position)
+    trinket.io/gloscript
+    
     
     
     
